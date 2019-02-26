667415926b27f722cbffbccf15f18a419e44c857
dominicnjh <dominicnjh@gmail.com>
2019-02-26 20:46:58 +0800

    Added content to profile, education, projects, experience and contacts sections. Added animation to projects and experience sections

---
+++ scripts/game.js
@@ -0,0 +1,77 @@
+var gamePattern = [];
+var userClickedPattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+var level = 0;
+var gameOver = false;
+var started = false;
+
+$(".btn").click(function() {
+  // Register user clicked button
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+
+  // Check if game is over
+  if (gameOver) {
+    $("h1").text("Game Over, Press Any Key to Restart");
+    gameOverState();
+  }
+});
+
+$(document).keypress(function() {
+  if (!started) {
+    started = true;
+    $("h1").text("Level " + level);
+    nextSequence();
+  }
+});
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (currentLevel === (gamePattern.length - 1)) {
+      setTimeout(nextSequence, 500);
+      userClickedPattern = [];
+    }
+  } else {
+    gameOver = true;
+  }
+}
+
+function startOver() {
+  gamePattern = [];
+  userClickedPattern = [];
+  level = 0;
+  started = false;
+  gameOver = false;
+}
+
+function gameOverState() {
+  $("body").addClass("game-over").delay(200).queue(function() {
+    $(this).removeClass("game-over").dequeue();
+  });
+  playSound("wrong");
+  startOver();
+}
+
+function nextSequence() {
+  level++;
+  $("h1").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(colour) {
+  new Audio("sounds/" + colour + ".mp3").play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed").delay(100).queue(function() {
+    $(this).removeClass("pressed").dequeue();
+  });
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,29 @@
-$(window).on("scroll", function () {
-    const winT = $(window).scrollTop();
-    const winH = $(window).height();
-    const skillsT = $("#skills").offset().top;
+$(function () {
+    // NavBar Smooth Scrolling Animation
+    $("#nav a").click(function (e) {
+        e.preventDefault();
+        $('html,body').scrollTo(this.hash, this.hash);
+    });
 
-    if (winT + winH > skillsT) {
-        $(".bar").each(function () {
-            $(this).find(".barfill").animate({
-                width: $(this).attr("data-percent")
-            }, 1000);
-        });
-    }
+    // Experience
+    $(".project").hover(function () {
+        $(this).find(".project-details").fadeIn(500);
+    }, function () {
+        $(this).find(".project-details").fadeOut(500);
+    });
+
+    // Skills Bar Animation
+    $(window).on("scroll", function () {
+        const winT = $(window).scrollTop();
+        const winH = $(window).height();
+        const skillsT = $("#skills").offset().top;
+
+        if (winT + winH > skillsT) {
+            $(".bar").each(function () {
+                $(this).find(".barfill").animate({
+                    width: $(this).attr("data-percent")
+                }, 1000);
+            });
+        }
+    });
 });
