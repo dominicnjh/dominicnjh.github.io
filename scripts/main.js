9e96ced93cb32e233341e5f6e3300c70e2895d98
dominicnjh <dominicnjh@gmail.com>
2019-03-14 23:30:46 +0800

    Edited smooth scrolling feature of navbar to various sections of the webpage.

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,42 @@
 $(function () {
-    // NavBar Fixed Position after Education Section
-    $("#education").waypoint(function(direction) {
+    // NavBar
+    // Fixed Position after Education Section
+    $("#education").waypoint(function (direction) {
         if (direction === "down") {
             $("nav").addClass("fixed-top");
         } else {
             $("nav").removeClass("fixed-top");
         }
     }, {
-        offset: "10%"
-    });
+            offset: "10%"
+        });
 
-    // NavBar Smooth Scrolling Animation
-    $("#nav a").click(function (e) {
-        e.preventDefault();
-        $('html,body').scrollTo(this.hash, this.hash);
+    // Smooth Scrolling Animation
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
     });
 
     // Experience
